a8412ef1043c89485662f7a6688a11ece305feaa
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-04-30 16:10:52 -0300

    feat: classes de exceção ParametroInvalido e ValorDeTransacaoNegativo criadas

--- src/main/java/br/edu/ifal/gqso/Conta.java
+++ src/main/java/br/edu/ifal/gqso/Conta.java
@@ -2,6 +2,8 @@ package br.edu.ifal.gqso;
 
 public class Conta {
   public class SaldoInsuficiente extends Exception {}
+  public class ParametroInvalido extends Exception {}
+  public class ValorDeTransacaoNegativo extends ParametroInvalido {}
 
   private float saldo = 0;
 }
