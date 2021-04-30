154d6c691f2ad6f63be795b973314f7da806082c
Z1wolfmaster1 <suiciniv2016@outlook.com>
2021-04-30 16:53:49 -0300

    fix: Trocado o tipo de exceção no throw de deposito

--- src/main/java/br/edu/ifal/gqso/Conta.java
+++ src/main/java/br/edu/ifal/gqso/Conta.java
@@ -2,19 +2,40 @@ package br.edu.ifal.gqso;
 
 public class Conta {
   public class SaldoInsuficiente extends Exception {}
+
   public class ValorDeTransacaoNegativo extends Exception {}
 
   private double saldo = 0;
 
   public double deposito(double valor) throws ValorDeTransacaoNegativo {
     try {
-    if (valor < 0) {
-      throw new Exception();
-    }
+      if (valor < 0) {
+        throw new ValorDeTransacaoNegativo();
+      }
 
       saldo += valor;
-    } catch (Exception e) {
+    } catch (ValorDeTransacaoNegativo e) {
       throw new ValorDeTransacaoNegativo();
+    }
+
+    return saldo;
+  }
+
+  public double saque(double valor) throws ValorDeTransacaoNegativo, SaldoInsuficiente {
+    try {
+      if (valor < 0) {
+        throw new ValorDeTransacaoNegativo();
+      }
+
+      if (saldo - valor < 0) {
+        throw new SaldoInsuficiente();
+      }
+
+      saldo -= valor;
+    } catch (ValorDeTransacaoNegativo v) {
+        throw new ValorDeTransacaoNegativo();
+    } catch (SaldoInsuficiente s) {
+        throw new SaldoInsuficiente();
     }
 
     return saldo;
